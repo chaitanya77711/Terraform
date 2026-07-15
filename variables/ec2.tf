d1bc5220daf008751ee9eac0b934102cfad0aa56
Chaitanya <chaitanyachaitu157@gmail.com>
2026-07-15 22:21:02 +0530

    ansible

---
+++ variables/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = var.tags
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = var.sg_group
+  description = var.sg_group_discription
+
+  egress {
+    from_port        = var.sg_from_port
+    to_port          = var.sg_to_port
+    protocol         = var.sg_protocol
+    cidr_blocks      = var.sg_cidr_block
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = var.sg_from_port
+    to_port          = var.sg_to_port
+    protocol         = var.sg_protocol
+    cidr_blocks      = var.sg_cidr_block
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = var.sg_tags
+}
---
+++ variables/variables.tf
@@ -0,0 +1,62 @@
+variable "ami_id" {
+    type = string
+    default = "ami-0220d79f3f480ecf5"
+    description = "RHEL 9 image"
+}
+
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+
+variable "tags" {
+    type = map
+    default = {
+        Name = "variables-demo"
+        project = "roboshop"
+        terraform = "true"
+        environment = "dev"
+    } 
+}
+
+variable "sg_group" {
+    type = string
+    default = "terraform-apply-all"
+}
+
+variable "sg_group_discription" {
+    type = string
+    default = "Allow TLS inbound traffic and all outbound traffic"
+}
+
+variable "sg_from_port" {
+    type = number
+    default = 0
+}
+
+variable "sg_to_port" {
+    type = number
+    default = 0
+}
+
+variable "sg_protocol" {
+    type = number
+    default = -1
+}
+
+variable "sg_cidr_block" {
+    type = list
+    default = ["0.0.0.0/0"]
+}
+
+variable "sg_tags" {
+    type = map
+    default = {
+    Name = "terraform-apply-all"
+    project = "roboshop"
+    terraform = "true"
+    environment = "dev"
+}
+}
