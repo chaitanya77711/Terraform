faac58b299e74a60e5d19aa97f2e41d92d08a0a8
Chaitanya <chaitanyachaitu157@gmail.com>
2026-07-21 17:30:30 +0530

    ansible

---
+++ Data source/data.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "RoboShop" {
+  most_recent = true
+  owners      = ["152537192595"]
+
+  filter {
+    name   = "name"
+    values = ["Redhat-9-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
